5d93fc46d40d3ab2435a780f91cb1f6292196c8a
nastyfizzle <nastassia.svistunova@gmail.com>
2021-10-17 21:47:54 +0300

    TMS-18 login steps updated

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -2,24 +2,28 @@ package steps;
 
 import io.qameta.allure.Step;
 import pages.LoginPage;
+import pages.ProjectsPage;
 
 public class LoginSteps {
 
     LoginPage loginPage;
+    ProjectsPage projectsPage;
 
     public LoginSteps() {
         loginPage = new LoginPage();
+        projectsPage = new ProjectsPage();
     }
 
     @Step("Authorize with valid credentials: {username}, {password}")
-    public void login(String username, String password) {
+    public ProjectsPage login(String username, String password) {
         loginPage
                 .open()
                 .validLogin(username, password)
                 .isOpened();
+        return projectsPage;
     }
 
-    @Step("Authorize with invalid credentials")
+    @Step("Authorize with invalid credentials: {username}, {password}")
     public LoginPage login(String username, String password, String errorText) {
         loginPage
                 .open()
